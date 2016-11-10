100e2d4b21f478fc8c85a8eb77a2b9ad3d6fd543
Harry <harrywye@gmail.com>
2016-11-10 00:24:24 -0800

    Implemented multi-foreground - single-background image function

--- CVDemo.Image/CVDemo.Image.Overlay/CVDemo.Image.Overlay.cpp
+++ CVDemo.Image/CVDemo.Image.Overlay/CVDemo.Image.Overlay.cpp
@@ -10,6 +10,7 @@
 #include <sstream>
 #include <stdexcept>
 // #include <boost/program_options.hpp>
+// #include <boost/filesystem.hpp>
 #include "CVDemo.Image.ProgramOptions.hpp"
 #include "CVDemo.Image.OverlayProcessor.hpp"
 
@@ -21,25 +22,38 @@ using namespace cv;
 using namespace std;
 using namespace cvdemo::image;
 // namespace po = boost::program_options;
+// using namespace boost::filesystem;
 
 
 // temporary
 vector<string> get_all_file_names_within_folder(string folder)
 {
 	vector<string> names;
+
+	// ????
 	string search_path = folder + "/*.png";
-	WIN32_FIND_DATA fd;
-	HANDLE hFind = ::FindFirstFile((LPCWSTR) search_path.c_str(), &fd);   // ????
+	WIN32_FIND_DATAA fd;
+	HANDLE hFind = ::FindFirstFileA(search_path.c_str(), &fd);   // ????
 	if (hFind != INVALID_HANDLE_VALUE) {
 		do {
 			// read all (real) files in current folder
 			// , delete '!' read other 2 default folder . and ..
 			if (!(fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-				names.push_back((LPCSTR) fd.cFileName);   // ????
+				// names.push_back(fd.cFileName);   // ????
+				names.push_back(folder + "/" + fd.cFileName);   // ????
 			}
-		} while (::FindNextFile(hFind, &fd));
+		} while (::FindNextFileA(hFind, &fd));
 		::FindClose(hFind);
 	}
+
+	//for (auto i = directory_iterator(folder); i != directory_iterator(); i++)
+	//{
+	//	if (!is_directory(i->path()))
+	//	{
+	//		names.push_back(i->path().filename().string());
+	//	}
+	//}
+
 	return names;
 }
 
